38ea97ebdfae21b1c321c2e7767a991ff36877fb
Bimochanacharya06 <bimochan081b@asm.edu.np>
2026-02-21 21:20:48 +0545

    Delete urls (1).py

--- urls (1).py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChatView, HealthView
-
-urlpatterns = [
-    path('chat/', ChatView.as_view(), name='chat'),
-    path('chat/health/', HealthView.as_view(), name='chat-health'),
-]
